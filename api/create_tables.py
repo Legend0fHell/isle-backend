97860c11fb81f5f852e668ee321b498be4022c67
Bui-Minh-Quan <buiminhquandz2005@gmail.com>
2025-05-25 01:40:30 +0700

    data: regenerate question data and create demo course content

--- api/create_tables.py
+++ api/create_tables.py
@@ -2,7 +2,9 @@ import sys
 import os 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+from models.question import Question, LessonQuestion 
+from models.alphabet import ASLCharacter 
+from models.user import User, UserLessonProgress, UserQuestionAnswer
 from database import Base, engine
 
 
