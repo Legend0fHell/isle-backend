f784aa621971faad70e70e0049a0f2673670096a
Phạm Nhật Quang <pnquang05@gmail.com>
2025-05-26 18:34:03 +0700

    ui: About page optimized

--- site/src/app/about/page.tsx
+++ site/src/app/about/page.tsx
@@ -21,7 +21,7 @@ const AboutPage = () => {
 
                 <div className="absolute top-[157px] left-[0px] w-[1282px] h-[617px]">
                     <Image className="absolute top-[25px] left-[661px] bg-gray-300 rounded-lg"
-                        src="/Pham Nhat Quang.png"
+                        src="/Pham Nhat Quang.jpg"
                         alt="about"
                         width={617} height={617} />
                     <div className="absolute top-[0px] left-[0px] w-[624px] h-[617px]">
@@ -32,11 +32,7 @@ const AboutPage = () => {
                         <div className="absolute top-[101px] left-[0px] w-[624px] h-[516px]">
                             <span className="absolute top-[0px] left-[0px] text-black w-[624px] h-[36px] opacity-60"
                                 style={{ fontSize: "24px", fontWeight: 400 }}>
-                                Job: AI developer, Leader
-                            </span>
-                            <span className="absolute top-[60px] left-[0px] text-black w-[624px] h-[72px] opacity-60"
-                                style={{ fontSize: "24px", fontWeight: 400 }}>
-                                Training model, Back-end developing and merging with Front-end
+                                Job: Leader, AI researcher, ML & Database backend developer
                             </span>
                             <span className="absolute top-[156px] left-[0px] text-black w-[624px] h-[36px] opacity-60"
                                 style={{ fontSize: "24px", fontWeight: 400 }}>
@@ -57,7 +53,7 @@ const AboutPage = () => {
 
                 <div className="absolute top-[943px] left-[0px] w-[1282px] h-[617px]">
                     <Image className="absolute top-[25px] left-[0px] bg-gray-300 rounded-lg"
-                        src="/Bui Minh Quan.png"
+                        src="/Bui Minh Quan.jpg"
                         alt="about"
                         width={617} height={617} />
                     <div className="absolute top-[0px] left-[661px] w-[624px] h-[617px]">
@@ -68,11 +64,7 @@ const AboutPage = () => {
                         <div className="absolute top-[101px] left-[0px] w-[624px] h-[516px]">
                             <span className="absolute top-[0px] left-[0px] text-black w-[624px] h-[36px] opacity-60"
                                 style={{ fontSize: "24px", fontWeight: 400 }}>
-                                Job: AI developer, BE developer
-                            </span>
-                            <span className="absolute top-[60px] left-[0px] text-black w-[624px] h-[72px] opacity-60"
-                                style={{ fontSize: "24px", fontWeight: 400 }}>
-                                Training model, Back-end developing
+                                Job: AI researcher, Database & Backend developer
                             </span>
                             <span className="absolute top-[156px] left-[0px] text-black w-[624px] h-[36px] opacity-60"
                                 style={{ fontSize: "24px", fontWeight: 400 }}>
@@ -93,7 +85,7 @@ const AboutPage = () => {
 
                 <div className="absolute top-[1726px] left-[0px] w-[1282px] h-[617px]">
                     <Image className="absolute top-[25px] left-[661px] bg-gray-300 rounded-lg"
-                        src="/Phan Quang Truong.png"
+                        src="/Phan Quang Truong.jpg"
                         alt="about"
                         width={617} height={617} />
                     <div className="absolute top-[0px] left-[0px] w-[624px] h-[617px]">
@@ -104,11 +96,7 @@ const AboutPage = () => {
                         <div className="absolute top-[101px] left-[0px] w-[624px] h-[516px]">
                             <span className="absolute top-[0px] left-[0px] text-black w-[624px] h-[36px] opacity-60"
                                 style={{ fontSize: "24px", fontWeight: 400 }}>
-                                Job: FE Developer
-                            </span>
-                            <span className="absolute top-[60px] left-[0px] text-black w-[624px] h-[72px] opacity-60"
-                                style={{ fontSize: "24px", fontWeight: 400 }}>
-                                Front-end developing and merging with Back-end
+                                Job: FE Developer, Document & Report writer
                             </span>
                             <span className="absolute top-[156px] left-[0px] text-black w-[624px] h-[36px] opacity-60"
                                 style={{ fontSize: "24px", fontWeight: 400 }}>
